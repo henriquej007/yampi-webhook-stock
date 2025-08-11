95dc85ac569bb8d212c8620700bf1398318b11db
henriquej007 <achadosrick@gmail.com>
2025-08-11 11:05:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import fetch from "node-fetch";
-import axios from 'axios';
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -109,6 +109,7 @@ app.post("/webhook", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
+
 
 
 
